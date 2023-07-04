c4018449ebd31b2cd2ed51a8aa06dce0b5033e2a
wbhaao <a31620504@gmail.com>
2023-07-05 07:59:12 +0900

    버튼 이벤트

--- App.js
+++ App.js
@@ -1,57 +1,28 @@
-import * as Location from "expo-location";
-import React, { useEffect, useState } from "react";
-import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
-  const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState();
-  const [ok, setOk] = useState(true);
-  const ask = async () => {
-    const { granted } = await Location.requestForegroundPermissionsAsync();
-    if (!granted) {
-      setOk(false);
-    }
-    const {
-      coords: { latitude, longitude },
-    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-    const location = await Location.reverseGeocodeAsync(
-      { latitude, longitude },
-      { useGoogleMaps: false }
-    );
-    setCity(location[0].city);
-  };
-  useEffect(() => {
-    ask();
-  }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>{city}</Text>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableOpacity>
       </View>
-      <ScrollView
-        pagingEnabled
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}
-      >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-      </ScrollView>
     </View>
   );
 }
@@ -59,29 +30,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
-  city: {
-    flex: 1.2,
-    justifyContent: "center",
-    alignItems: "center",
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
   },
-  cityName: {
-    fontSize: 58,
-    fontWeight: "500",
-  },
-  weather: {},
-  day: {
-    width: SCREEN_WIDTH,
-    alignItems: "center",
-  },
-  temp: {
-    marginTop: 50,
+  btnText: {
+    fontSize: 38,
     fontWeight: "600",
-    fontSize: 178,
-  },
-  description: {
-    marginTop: -30,
-    fontSize: 60,
+    color: "white",
   },
 });
